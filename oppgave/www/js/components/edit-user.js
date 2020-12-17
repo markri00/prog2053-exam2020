d3dd5c6b537d4670367594d9fc6c5e177b530e2f
markri00 <72941427+markri00@users.noreply.github.com>
2020-12-17 11:50:23 +0100

    Update edit-user.js

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,25 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  render() {
+    return html`
+      <div>
+      	<form action="api/updateUser.php" method="POST">
+      		<p>Brukernavnet per er nå <b>${this.user.uname}</b></p>
+      		<li>Nytt brukernavn: <input type="text" id="uname" name="uname"></li>
+      		<p>Fornavnet per er nå <b>${this.user.firstName}</b></p>
+      		<li>Nytt fornavn: <input type="text" id="firstName" name="firstName"></li>
+      		<p>Etternavnet per er nå <b>${this.user.lastName}</b></p>
+      		<li>Nytt etternavn: <input type="text" id="lastName" name="lastName"></li>
+      		<li>Endre passord: <input type="text" id="pwd" name="pwd"></li>
+      		<br>
+      		<p>Bruker ID'en er <b>${this.user.uid}</b></p>
+      		<li>Ny ID: <input type="text" id="uid" name="uid"></li>
+      		<button type="submit">Oppdater informasjon</button>
+      	</form>
+      </div>
+    `;
+  }
 
 }
 customElements.define('edit-user', EditUser);
